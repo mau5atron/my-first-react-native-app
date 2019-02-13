8f15bd9ea444162fdff28c5bd653044a31015ecf
Gabriel Betancourt <mau5atron-dev@Gabriels-MacBook-Pro.local>
2019-02-13 12:01:30 -0500

    created style constant for text

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{color: 'white'}}>Wow! This is my first react native app!</Text>
+      <View style={stylesView.container}>
+        <Text style={stylesText.container}>Wow! This is my first react native app!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const stylesView = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const stylesText = StyleSheet.create({
+  container: {
+    color: "white",
   },
 });
