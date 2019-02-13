2ad8e042ef08f37bc1835d8e0f0f3b642bb05c25
Gabriel Betancourt <mau5atron-dev@Gabriels-MacBook-Pro.local>
2019-02-13 12:04:35 -0500

    refactored style to add single container for text and single container for view

--- App.js
+++ App.js
@@ -4,24 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={stylesView.container}>
-        <Text style={stylesText.container}>Wow! This is my first react native app!</Text>
+      <View style={styles.containerView}>
+        <Text style={styles.containerText}>
+          Wow! This is my first react native app!
+        </Text>
       </View>
     );
   }
 }
 
-const stylesView = StyleSheet.create({
-  container: {
+const styles = StyleSheet.create({
+  containerView: {
     flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
 
-const stylesText = StyleSheet.create({
-  container: {
-    color: "white",
+  containerText: {
+    color: 'white',
   },
 });
+
